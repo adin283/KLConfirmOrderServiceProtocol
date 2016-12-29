7e8da20bb8e71e7702b85e2aa90ff498e24a3c8d
祝文博 <zhuwenbo@zhujia360.com>
2016-12-29 17:29:35 +0800

    first commit

---
+++ KLConfirmOrderServiceProtocol/Classes/KLConfirmOrderServiceProtocol.h
@@ -0,0 +1,15 @@
+//
+//  KLConfirmOrderServiceProtocol.h
+//  KLConfirmOrderServiceProtocol
+//
+//  Created by Kevin on 2016/12/29.
+//  Copyright © 2016年 KevinLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol KLConfirmOrderServiceProtocol <NSObject>
+
+- (UIViewController *)confirmOrderViewControllerWithGoodsId:(NSString *)goodsId sureComplete:(dispatch_block_t)sureComplete;
+
+@end
